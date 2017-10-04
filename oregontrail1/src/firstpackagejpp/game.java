579761fd365762246b32bdee89d12ddde6fc9b5f
parhe <parhe@DESKTOP-4TBDPQR>
2017-10-04 17:39:56 -0600

    10 - 04 - 17 Project Created

--- oregontrail1/src/firstpackagejpp/game.java
+++ oregontrail1/src/firstpackagejpp/game.java
@@ -5,10 +5,49 @@
  */
 package firstpackagejpp;
 
+import java.io.Serializable;
+
 /**
  *
  * @author parhe
  */
-public class game {
+public class game implements Serializable {
+    
+    private double totalTime;
+    private int noPeople;
+    private Player player; 
+
+    public game() {
+    }
+
+    public double getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(double totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    public int getNoPeople() {
+        return noPeople;
+    }
+
+    public void setNoPeople(int noPeople) {
+        this.noPeople = noPeople;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    
+
+    
+    
+    
     
 }
