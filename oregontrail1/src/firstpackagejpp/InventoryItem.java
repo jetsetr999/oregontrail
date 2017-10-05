9cb122cb4c3526a5f29ca0ba969523b85e76f1e8
Darrin <Darrin@192.168.0.8>
2017-10-05 17:19:49 -0600

    added string() and equals() after the getter

--- oregontrail1/src/firstpackagejpp/InventoryItem.java
+++ oregontrail1/src/firstpackagejpp/InventoryItem.java
@@ -6,6 +6,7 @@
 package firstpackagejpp;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
@@ -58,6 +59,54 @@ public class InventoryItem implements Serializable
     public void setPricePerUnit(double pricePerUnit) {
         this.pricePerUnit = pricePerUnit;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + Objects.hashCode(this.name);
+        hash = 23 * hash + this.quantityInStock;
+        hash = 23 * hash + this.requiredAmount;
+        hash = 23 * hash + this.weightPerUnit;
+        hash = 23 * hash + (int) (Double.doubleToLongBits(this.pricePerUnit) ^ (Double.doubleToLongBits(this.pricePerUnit) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final InventoryItem other = (InventoryItem) obj;
+        if (this.quantityInStock != other.quantityInStock) {
+            return false;
+        }
+        if (this.requiredAmount != other.requiredAmount) {
+            return false;
+        }
+        if (this.weightPerUnit != other.weightPerUnit) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.pricePerUnit) != Double.doubleToLongBits(other.pricePerUnit)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "InventoryItem{" + "name=" + name + ", quantityInStock=" + quantityInStock + ", requiredAmount=" + requiredAmount + ", weightPerUnit=" + weightPerUnit + ", pricePerUnit=" + pricePerUnit + '}';
+    }
+    
+    
     
     
 }
