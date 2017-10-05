741ea6c7e53184336ebce3b9f9998e4769dd8a99
Darrin <Darrin@192.168.0.8>
2017-10-05 17:22:47 -0600

    added string() and equals() after the getter

--- oregontrail1/src/firstpackagejpp/WeatherEvent.java
+++ oregontrail1/src/firstpackagejpp/WeatherEvent.java
@@ -22,5 +22,37 @@ public class WeatherEvent extends Event implements Serializable
     public void setExtraDelay(int extraDelay) {
         this.extraDelay = extraDelay;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + this.extraDelay;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final WeatherEvent other = (WeatherEvent) obj;
+        if (this.extraDelay != other.extraDelay) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "WeatherEvent{" + "extraDelay=" + extraDelay + '}';
+    }
   
+    
+    
 }
