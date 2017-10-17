778e5a072bd09d7f261fa49b2f3cedf91a0f4108
parhe <parhe@DESKTOP-4TBDPQR>
2017-10-17 17:39:28 -0600

    10 - 17 - 17 added test cases (Inventory Control)

--- oregontrail1/test/oregontrail/control/InventoryControlTest.java
+++ oregontrail1/test/oregontrail/control/InventoryControlTest.java
@@ -5,38 +5,47 @@
  */
 package oregontrail.control;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
  *
- * @author Kyle Jones
+ * @author parhe
  */
 public class InventoryControlTest {
     
     public InventoryControlTest() {
     }
+
     /**
-     * Test of getSubTotal method, of class InventoryControl.
+     * Test 1
      */
     @Test
-    public void testGetSubTotal() {
+    public void test1GetSubTotal() {
         System.out.println("getSubTotal");
-        double weight = 0.0;
-        long amount = 0L;
-        double pricePerUnit = 0.0;
-        double onHandItem = 0.0;
-        double itemQuantity = 0.0;
+        System.out.println("Test Case 1");
+        double pricePerUnit = 2;
+        double itemQuantity = 9;
         InventoryControl instance = new InventoryControl();
-        double expResult = 0.0;
-        double result = instance.getSubTotal(weight, amount, pricePerUnit, onHandItem, itemQuantity);
+        double expResult = 18;
+        double result = instance.getSubTotal(pricePerUnit, itemQuantity);
         assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
     
-}
+    /**
+     * Test 2
+     */
+    @Test
+    public void test2GetSubTotal() {
+        System.out.println("getSubTotal");
+        System.out.println("Test Case 2");
+        double pricePerUnit = -5;
+        double itemQuantity = 4;
+        InventoryControl instance = new InventoryControl();
+        double expResult = -1;
+        double result = instance.getSubTotal(pricePerUnit, itemQuantity);
+        assertEquals(expResult, result, 0.0);
+        
+    }
+}   
