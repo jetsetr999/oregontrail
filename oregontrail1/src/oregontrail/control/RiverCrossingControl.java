2e98a19372856e97d5f6e266a5eafddfd0d2bf7d
parhe <parhe@DESKTOP-4TBDPQR>
2017-10-21 22:57:30 -0600

    10 - 21 - 17 rivercrosstest1 (Inventory Control)

--- oregontrail1/src/oregontrail/control/RiverCrossingControl.java
+++ oregontrail1/src/oregontrail/control/RiverCrossingControl.java
@@ -5,10 +5,38 @@
  */
 package oregontrail.control;
 
+
 /**
  *
  * @author Kyle Jones
  */
 public class RiverCrossingControl {
+
+    private double depth;
+    private double weight;
+    private double speed;
+    private double multiplyer;
     
-}
+    public double calcSupplyDrop(double amntSuppliesDropped, double currentSupplies, double depth, double multiplyer, double speed, double weight, double amount) {
+        
+        if (amount < 0 || weight < 0 || speed < 0 || weight > 300 || amount > 300 || speed > 5) {
+            
+            System.out.println("\nThe weight and amount must be greater than 0 and the weight and amount must be less than or equal to 300! Speed can only be 0 through 5!"); 
+            return -1;
+        }
+        
+        if (depth >= 1 || depth <= 10 || weight >= 1 || weight <= 10 || speed == 0 || speed == 1) {
+            multiplyer = .005;
+        } 
+        else if (depth >= 11 || depth <= 100 ||weight >= 11 || weight <= 100 || speed == 2 || speed == 3)  {
+            multiplyer = .008;
+        }
+        else if (depth >= 101 || depth <= 299 || weight >= 101 || weight <= 299 || speed == 4 || speed == 5)
+            multiplyer = .01;
+        return 0;
+        }
+        double amntSuppliesDropped = depth * multiplyer + speed * multiplyer + weight * multiplyer;
+
+    }
+    
+
