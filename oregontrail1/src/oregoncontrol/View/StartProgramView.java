5cf3ef351912f8bee0d6a67a2ff7c0156a3d8571
Darrin <Darrin@192.168.0.8>
2017-10-23 16:31:00 -0600

    Added view package and start control

---
+++ oregontrail1/src/oregoncontrol/View/StartProgramView.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oregoncontrol.View;
+
+/**
+ *
+ * @author Darrin
+ */
+public class StartProgramView {
+    
+    displayStartProgramView(){
+        
+     // banner Darrin
+     // player name - Brandon
+     // control function to create player object - Kyle
+     // welcome message - main menu Darrin
+     
+     
+    }
+    
+}
