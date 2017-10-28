0cc888c0c766292e7c10272fe184a67638e7faab
Darrin <Darrin@Da_Laptop>
2017-10-27 19:03:26 -0600

    Adjustments to helpMenu

--- oregontrail1/src/oregoncontrol/View/HelpMenuView.java
+++ oregontrail1/src/oregoncontrol/View/HelpMenuView.java
@@ -14,98 +14,98 @@ import oregontrail1.OregonTrail1;
  * @author parhe
  */
 public class HelpMenuView {
-    
+
     private String menu;
     private String promptCommand;
 
     public HelpMenuView() {
         this.menu = "\n"
-                  + "\n---------------------------------"
-                  + "\n| Help Menu                     |"
-                  + "\n---------------------------------"
-                  + "\nG - What is the goal of the game?"
-                  + "\nM - How to move"
-                  + "\nT - Tips"  
-                  + "\nR - Return to previous menu" 
-                  + "\n---------------------------------";
-                            
-                
+                + "\n\t ---------------------------------"
+                + "\n\t | Help Menu                     |"
+                + "\n\t ---------------------------------"
+                + "\n\t G - Game strategy"
+                + "\n\t M - Learn How to move"
+                + "\n\t A - Game assists or Helpful hints"
+                + "\n\t P - Return to previous menu"
+                + "\n\t ---------------------------------";
+
     }
 
     public void displayHelpMenuView() {
-        
+
         boolean done = false;
         do {
             String menuOption = this.getMenuOption();
-            if (menuOption.toUpperCase().equals("R"))
+            if (menuOption.toUpperCase().equals("P")) {
                 return;
-            
+            }
+
             done = this.doAction(menuOption);
         } while (!done);
-    
+
     }
 
     private String getMenuOption() {
-        
+
         Scanner keyboard = new Scanner(System.in);
-	String value = "G";
-	boolean valid = false;
-	
-	while (!valid) {
+        String value = "G";
+        boolean valid = false;
+
+        while (!valid) {
             System.out.println("\n" + this.promptCommand);
-		
+
             value = keyboard.nextLine();
             value = value.trim();
-		
+
             if (value.length() < 1) {
-		System.out.println("\nInvalid value: value can not be blank");
-		continue;
-		}
-		
-		break;
-	}
-	
-	return value;
+                System.out.println("\nInvalid value: value can not be blank");
+                continue;
+            }
+
+            break;
+        }
+
+        return value;
     }
 
     private boolean doAction(String choice) {
-        
+
         choice = choice.toUpperCase();
-        
+
         switch (choice) {
             case "G":
-                this.displayGameObjective();
+                this.displayGameStrategy();
                 break;
-            case "H":
-                this.displayControls();
+            case "M":
+                this.displayMovement();
                 break;
-            case "T":
-                this.displayTips();
+            case "A":
+                this.displayAssists();
                 break;
             default:
                 System.out.println("\n*** Invalid Selection *** Try Again");
                 break;
         }
-            return false;
+        return false;
     }
 
     private void startNewGame() {
         GameControl.createNewGame(OregonTrail1.getPlayer());
-        
+
         GameMenuView gameMenu = new GameMenuView();
         gameMenu.displayMenu();
     }
 
-    private void displayGameObjective() {
-        System.out.println("\n*** displayGameObjective stub function called ***");
+    private void displayGameStrategy() {
+        System.out.println("\n*** displayGameStrategy stub function called ***");
     }
 
-    private void displayControls() {
-        System.out.println("\n*** displayControls stub function called ***");
+    private void displayMovement() {
+        System.out.println("\n*** displayMovement stub function called ***");
     }
 
-    private void displayTips() {
-        System.out.println("\n*** displayTips stub function called ***");
+    private void displayAssists() {
+        System.out.println("\n*** displayAssists stub function called ***");
     }
-                   
+
 }
