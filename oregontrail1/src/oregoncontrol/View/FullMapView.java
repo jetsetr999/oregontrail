27cbf04cf0a52b27c8f3e2440ede3cb2f4554c4a
parhe <parhe@DESKTOP-4TBDPQR>
2017-11-01 21:36:53 -0600

    11 - 1 - 17 mod

---
+++ oregontrail1/src/oregoncontrol/View/FullMapView.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oregoncontrol.View;
+
+/**
+ *
+ * @author parhe
+ */
+class FullMapView {
+
+    void displayFullMapMenu() {
+        System.out.println("\n*** displayFullMapView stub function called ***");
+    }
+    
+}
