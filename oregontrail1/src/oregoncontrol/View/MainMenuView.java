0cfb70e65ee29b56abbe3a9f11d730d81858b8d8
parhe <parhe@DESKTOP-4TBDPQR>
2017-11-01 21:34:52 -0600

    11 - 1 - 17 MainMenuView Mods

--- oregontrail1/src/oregoncontrol/View/MainMenuView.java
+++ oregontrail1/src/oregoncontrol/View/MainMenuView.java
@@ -13,105 +13,141 @@ import oregontrail1.OregonTrail1;
  *
  * @author parhe
  */
-public class MainMenuView {
-    
-    private String menu;
-    private String promptCommand;
+public final class MainMenuView {
 
+    private String menu;
+    private final String promptCommand;
+    
     public MainMenuView() {
-        this.menu = "\n"
-                  + "\n------------------------------"
-                  + "\n| Main Menu                  |"
-                  + "\n------------------------------"
-                  + "\nN - Start new game"
-                  + "\nG - Get and start saved game"
-                  + "\nH - Get help on how to play the game"  
-                  + "\nS - Save game"
-                  + "\nQ - Quit"                
-                  + "\n------------------------------";
-                            
-                
+        
+        this.promptCommand = "\nPlease Make Selection: ";
+        
+        this.displayMainMenuBanner();
+        
+    }
+
+    /*
+     * We are leaving the commands for the Main Menu commands 
+     * the same as the instructions for ease of use.
+     */
+    public void displayMainMenuBanner() {
+               
+            System.out.println(
+                  "\n"
+                + "\n------------------------------"
+                + "\n| Main Menu                  |"
+                + "\n------------------------------"
+                + "\nN - Start new game"
+                + "\nG - Get and start saved game"
+                + "\nH - Get help on how to play the game"
+                + "\nS - Save game"
+                + "\nM - Map View"
+                + "\nQ - Quit"
+                + "\n------------------------------"
+            );
+
     }
 
     public void displayMainMenuView() {
-        
+
         boolean done = false;
         do {
             String menuOption = this.getMenuOption();
-            if (menuOption.toUpperCase().equals("Q"))
+            if (menuOption.toUpperCase().equals("Q")) {
                 return;
-            
+            }
+
             done = this.doAction(menuOption);
         } while (!done);
-    
+
     }
 
+    // 
     private String getMenuOption() {
-        
+
         Scanner keyboard = new Scanner(System.in);
-	String value = "N";
-	boolean valid = false;
-	
-	while (!valid) {
+        String value = "N";
+        boolean valid = false;
+
+        while (!valid) {
             System.out.println("\n" + this.promptCommand);
-		
+
             value = keyboard.nextLine();
             value = value.trim();
-		
+
             if (value.length() < 1) {
-		System.out.println("\nInvalid value: value can not be blank");
-		continue;
-		}
-		
-		break;
-	}
-	
-	return value;
+                System.out.println("\nInvalid value: value can not be blank");
+                continue;
+            }
+
+            break;
+        }
+
+        return value;
     }
 
     private boolean doAction(String choice) {
-        
-        choice = choice.toUpperCase();
-        
+
+        choice = choice.toUpperCase(); // lowercase will work for uppercase
+
         switch (choice) {
             case "N":
-                this.startNewGame();
+                this.startNewGame(); // Start New Game
                 break;
             case "G":
-                this.startExistingGame();
+                this.startExistingGame(); // Load Game
                 break;
             case "H":
-                this.displayHelpMenu();
+                this.displayHelpMenu(); // Help Menu
+                break;
+            case "M":
+                this.displayMapMenu(); // map menu
                 break;
             case "S":
-                this.saveGame();
+                this.saveGame(); // Save Game
                 break;
             default:
                 System.out.println("\n*** Invalid Selection *** Try Again");
                 break;
         }
-            return false;
+        return false;
     }
 
+    // calls the new game
     private void startNewGame() {
         GameControl.createNewGame(OregonTrail1.getPlayer());
-        
-        GameMenuView gameMenu = new GameMenuView();
-        gameMenu.displayMenu();
+
+        GameMenuView gameMenuView = new GameMenuView();
+        gameMenuView.displayGameMenuView();
     }
 
+    // existing game
     private void startExistingGame() {
         System.out.println("\n*** start existing game ***");
+
+        GameMenuView gameMenuView = new GameMenuView();
+        gameMenuView.displayGameMenuView();
     }
 
+    // calls the help menu
     private void displayHelpMenu() {
-        HelpMenuView helpMenuView = new HelpMenuView();
         
+        HelpMenuView helpMenuView = new HelpMenuView();
         helpMenuView.displayHelpMenuView();
+
     }
 
+    // stub function
     private void saveGame() {
         System.out.println("\n*** save game ***");
+        
     }
-                     
+
+    // calls the map menu
+    private void displayMapMenu() {
+
+        MapMenuView mapMenuView = new MapMenuView();
+        mapMenuView.displayMapMenuView();
+
+    }
 }
