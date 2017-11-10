873f23d121936e939748a692e5cd240192bc204b
parhe <parhe@DESKTOP-4TBDPQR>
2017-11-09 17:12:19 -0700

    Added Work

--- oregontrail1/src/oregoncontrol/View/GameMenuView.java
+++ oregontrail1/src/oregoncontrol/View/GameMenuView.java
@@ -5,53 +5,151 @@
  */
 package oregoncontrol.View;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Darrin
  */
+
 public class GameMenuView {
-
-    void displayMenu() {
-        System.out.println("\n"
-                + "\n\t------------------------------"
-                + "\n\t| Main Menu                  |"
-                + "\n\t------------------------------"
-                + "\n\tN - Start new game"
-                + "\n\tG - Get and start saved game"
-                + "\n\tH - Get help on how to play the game"
-                + "\n\tS - Save game"
-                + "\n\tM - Map View"
-                + "\n\tQ - Quit"
-                + "\n\t------------------------------");
-
+    
+    private String menu;
+    private final String promptCommand;
+    
+    public GameMenuView() {
+        
+    this.promptCommand = "\nPlease Make Selection: ";
+        
+    this.displayGameMenuBanner();
+        
     }
 
-    void displayMapMenu() {
-        System.out.println("\n"
-                + "\n\t ---------------------------------"
-                + "\n\t | Map Menu                     |"
-                + "\n\t ---------------------------------"
-                + "\n\t M - Full Map View"
-                + "\n\t D - Distance to next town, fort, or checkpoint"
-                + "\n\t E - Distance to final destination"
-                + "\n\t T - Distance Traveled "
-                + "\n\t P - Return to previous menu"
-                + "\n\t ---------------------------------");
-
+    public void displayGameMenuBanner() {
+        
+                System.out.println(
+                      "\n******************************************************"
+                    + "\n |Current State Information:                         |"
+                    + "\n Today is (place current date and time here)          "                                             
+                    + "\n Distance traveled is (place distance traveled)       "
+                    + "\n Remaining distance is (place remaining distance)     "
+                    + "\n (display surrounding towns, rivers, mountains, exc)  "
+                    + "\n (display events/scenes)                              "
+                    + "\n |Options:                                           |"
+                    + "\nT - View Team Status                                  "
+                    + "\nS - View Team Supplies                                "
+                    + "\nP - Set Team Pace                                     "
+                    + "\nA - Scene Menu (based upon active scene)              "
+                    + "\nM - View The Map                                      "
+                    + "\nH - Help Menu                                         "
+                    + "\nR - Return To Main Menu                               "
+                    + "\n******************************************************"
+                );
+    }
+    
+    public void displayGameMenuView() {
+        
+        boolean done = false;
+        do {
+            String menuOption = this.getMenuOption();
+            if (menuOption.toUpperCase().equals("Q"))
+                return;
+            
+            done = this.doAction(menuOption);
+        } while (!done);
+    
+    }
+    
+    private String getMenuOption() {
+        
+        Scanner keyboard = new Scanner(System.in);
+	String value = "N";
+	boolean valid = false; 
+	while (!valid) {
+            System.out.println("\n" + this.promptCommand);
+		
+            value = keyboard.nextLine();
+            value = value.trim();
+		
+            if (value.length() < 1) {
+		System.out.println("\nInvalid value: value can not be blank");
+		continue;
+		}
+		
+		break;
+	}
+	
+	return value;
+    }
+    
+    private boolean doAction(String choice) {
+        
+        choice = choice.toUpperCase();
+        
+        switch (choice) {
+            case "T":
+                this.viewTeamStatus();
+                break;
+            case "S":
+                this.viewTeamSupplies();
+                break;
+            case "P":
+                this.setTeamPace();
+                break;
+            case "A":
+                this.sceneMenu();
+                break;
+            case "M":
+                this.viewMap();
+                break;
+            case "H":
+                this.helpMenu();
+                break;
+            case "R":
+                this.returnToMainMenu();
+                break;
+            default:
+                System.out.println("\n*** Invalid Selection *** Try Again");
+                break;
+        }
+            return false;
     }
 
-    void displayHelpMenu() {
-        System.out.println("\n"
-                + "\n\t ---------------------------------"
-                + "\n\t | Help Menu                     |"
-                + "\n\t ---------------------------------"
-                + "\n\t G - Game strategy"
-                + "\n\t M - Learn How to move"
-                + "\n\t A - Game assists or Helpful hints"
-                + "\n\t D - Gsme definitions"
-                + "\n\t P - Return to previous menu"
-                + "\n\t ---------------------------------");
+    private void viewTeamStatus() {
+        
+        TeamStatusView teamStatusView = new TeamStatusView();
+        
+        teamStatusView.displayTeamStatusView();
+    }
 
+    private void viewTeamSupplies() {
+        System.out.println("\n*** Team Status Called");
+    }
+
+    private void setTeamPace() {
+        TeamPaceView teamPaceView = new TeamPaceView();
+        
+        teamPaceView.displayTeamPaceMenuView();
+    }
+
+    private void sceneMenu() {
+        System.out.println("\n*** Scene Menu Called");
+    }
+
+    private void viewMap() {
+        MapMenuView mapMenuView = new MapMenuView();
+        mapMenuView.displayMapMenuView();
+    }
+
+    private void helpMenu() {
+        HelpMenuView helpMenuView = new HelpMenuView();
+        
+        helpMenuView.displayHelpMenuView();
+    }
+
+    private void returnToMainMenu() {
+        MainMenuView mainMenuView = new MainMenuView();
+	mainMenuView.displayMainMenuView();
     }
 
 }
