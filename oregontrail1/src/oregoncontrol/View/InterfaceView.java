b9722d8abf6575fbc90eae53c2a60c95380c2791
parhe <parhe@DESKTOP-4TBDPQR>
2017-11-09 17:39:07 -0700

    Interface and View Class mods

---
+++ oregontrail1/src/oregoncontrol/View/InterfaceView.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oregoncontrol.View;
+
+/**
+ *
+ * @author parhe
+ */
+public interface InterfaceView {
+    
+    public void display();
+    public String getInput();
+    public boolean doAction(String value);
+    
+}
