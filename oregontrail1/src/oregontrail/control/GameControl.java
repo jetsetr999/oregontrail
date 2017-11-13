1b1f1aa21571b4ba68eac217bcf9e06356478b08
parhe <parhe@DESKTOP-4TBDPQR>
2017-11-13 16:46:55 -0700

    game control 1.1

--- oregontrail1/src/oregontrail/control/GameControl.java
+++ oregontrail1/src/oregontrail/control/GameControl.java
@@ -30,7 +30,27 @@ public class GameControl {
 }
 
     public static void createNewGame(Player player) {
-        System.out.println("\n*** createNewGame stub function called ***");
+        
+        if (player == null)
+            return -1;
+        
+        public static 
+                    
+  /*    game = create a new Game object
+    Save a reference to the Player object in the game
+    Save a reference to the game in the main class
+    actors = createActors()
+    Save the list of actors in the Game object
+    Assign an actor to the player
+    items = createItems()
+    Save the list of items in the game
+    map = createMap(noOfRows, noOfColumns)
+    IF map == null THEN
+    RETURN -1
+    ENDIF
+    Assign the map to the game
+    RETURN 1 // indicates success*/
+}
     }
 
     public static WeatherEvent createWeatherChoice(String weatherChoice) {
