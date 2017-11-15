cc2f74e5f487a19ac9a82107661ae710f03edf32
Darrin <Darrin@Da_Laptop>
2017-11-14 23:01:11 -0700

    minor adjustments added a few lines in gamecontrol

--- oregontrail1/src/oregontrail/control/GameControl.java
+++ oregontrail1/src/oregontrail/control/GameControl.java
@@ -6,6 +6,7 @@
 package oregontrail.control;
 
 import firstpackagejpp.Player;
+import oregontrail1.Game;
 import oregontrail1.OregonTrail1;
 
 /**
@@ -14,51 +15,66 @@ import oregontrail1.OregonTrail1;
  */
 public class GameControl {
 
-    public static Player createPlayer(String name) {
-        
-        if (name == null) {
-            return null;
-        }
-        
-        Player player = new Player();
-        player.setName(name);
-        
-        OregonTrail1.setPlayer(player);
-        
-        return player;   
-}
-
     public static int createNewGame(Player player) {
+
+       // create a new game
+        int returnValue = GameControl.createNewGame(OregonTrail1.getPlayer());
+        if (returnValue < 0){
+            System.out.println("Error - Failed to create new game");
+        }
         
         if (player == null) {
             return -1;
-        }       
-        
-        Game game = new Game();
-        game.setPlayer(player);
-        
-        OregonTrail1.currentGame(game);
-    /*Save a reference to the Player object in the game
-    Save a reference to the game in the main class
-    actors = createActors()
-    Save the list of actors in the Game object
-    Assign an actor to the player
-    items = createItems()
-    Save the list of items in the game
-    map = createMap(noOfRows, noOfColumns)
-    IF map == null THEN
-    RETURN -1
-    ENDIF
-    Assign the map to the game
-    RETURN 1 // indicates success*/
-}
+        }
 
+        Game game = new Game();
+        
+        
     
+        //    MainMenuView mainMenuView = new MainMenuView();
+        //  mainMenuView.display();
+
+    public static Player createPlayer(String name) {
+
+        if (name == null) {
+            return null;
+        }
+
+        Player player = new Player();
+        player.setName(name);
+
+        OregonTrail1.setPlayer(player);
+
+        return player;
     }
+
+    game.setPlayer (player);
+
+    OregonTrail1.currentGame (game);
+   
+}
+/* public static int createNewGame(Player player) {  
+if (player == null)  
+ return -1 
+game = create a new Game object 
+Save a reference to the Player object in the game 
+Save a reference to the game in the main class
+actors = createActors() 
+Save the list of actors in the Game object 
+Assign an actor to the player 
+items = createItems() 
+Save the list of items in the game 
+map = createMap(noOfRows, noOfColumns) 
+IF map == null THEN    
+RETURN -1 
+ENDIF 
+Assign the map to the game 
+RETURN 1 // indicates success } */
+
+
+}
 
     public static Actor[] createActors() {
 
     
 }
-
-
