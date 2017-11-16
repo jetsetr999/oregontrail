7b6dcf680509ee2c4c35c08ccd5cc42e54e630d9
parhe <parhe@DESKTOP-4TBDPQR>
2017-11-16 16:50:35 -0700

    game edit

--- oregontrail1/src/oregontrail1/Game.java
+++ oregontrail1/src/oregontrail1/Game.java
@@ -5,10 +5,30 @@
  */
 package oregontrail1;
 
+import firstpackagejpp.InventoryItem;
+import firstpackagejpp.Map;
+import firstpackagejpp.Player;
+
 /**
  *Hello Y'all
  * @author Darrin
  */
 public class Game {
+
+    public void setPlayer(Player player) {
+        System.out.println("\n*** set player ***");
+    }
+
+    public void setCompanion1(String string) {
+        System.out.println("\n*** set companion1 ***");
+    }
+
+    public void setInventory(InventoryItem[] items) {
+        System.out.println("\n*** set inventory ***");
+    }
+
+    public void setMap(Map map) {
+        System.out.println("\n*** set map ***");
+    }
     
 }
