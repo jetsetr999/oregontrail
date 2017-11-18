9d72114be9936c964cbdd988ca7e09e3493e569d
parhe <parhe@DESKTOP-4TBDPQR>
2017-11-17 18:57:42 -0700

    Scene Type

---
+++ oregontrail1/src/firstpackagejpp/Enums/SceneType.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstpackagejpp.Enums;
+import java.io.Serializable;
+
+/**
+ *
+ * @author parhe
+ */
+public enum SceneType implements Serializable {
+    
+        Fort,
+        Hotel,
+        Regular,
+        Resource,
+        RiverCrossing,
+        Town,
+        TrailStop,
+        Independence,
+        Landmark,
+        Mountain,
+        Shop,
+        Trail,
+        Wagon,
+        Death,
+        Supplies,
+
+} 
+
+
+    
