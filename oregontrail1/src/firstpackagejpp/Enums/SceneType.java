35c24aa8189a2d75286df2136cfd4073b30843cd
parhe <parhe@DESKTOP-4TBDPQR>
2017-11-17 21:08:18 -0700

    Scene Type edit

--- oregontrail1/src/firstpackagejpp/Enums/SceneType.java
+++ oregontrail1/src/firstpackagejpp/Enums/SceneType.java
@@ -12,26 +12,63 @@ import java.io.Serializable;
  */
 public enum SceneType implements Serializable {
     
-        Start,
-        Independence,
-        Fort,
-        Hotel,
-        Regular,
-        Resource,
-        RiverCrossing,
-        Town,
-        TrailStop,
-        Landmark,
-        Mountain,
-        Shop,
-        Trail,
-        Wagon,
-        Death,
-        Supplies,
-        Questions, 
-        End,
+        Start("Scene1", "Start of game"),
+        Independence("Scene2", "Independence"),
+        Fort("Scene3", "Fort"),
+        Hotel("Scene4", "Hotel"),
+        Regular("Scene5", "Regular"),
+        Resource("Scene6", "Resource"),
+        RiverCrossing("Scene7", "River Crossing"),
+        Town("Scene8", "Town"),
+        TrailStop("Scene9", "Trail Stop"),
+        Landmark("Scene10", "Land Mark"),
+        Mountain("Scene11", "Mountain"),
+        Shop("Scene12", "Shop"),
+        Trail("Scene13", "Trail"),
+        Wagon("Scene14", "Wagon"),
+        Death("Scene15", "Death"),
+        Supplies("Scene16", "Supplies"),
+        Questions("Scene17", "Questions"),
+        End("Scene18", "End of game"),;
+        
+        
+    private String scene;
+    private String description;
+    //private double travelTime;
 
-} 
+    SceneType(String scene, String description){
+        this.scene = scene;
+        this.description = description;
+        //this.travelTime = TravelTime;
+          
+    }
+
+    public String getScene() {
+        return scene;
+    }
+
+    public void setScene(String scene) {
+        this.scene = scene;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /*public double getTravelTime() {
+        return travelTime;
+    }
+
+    public void setTravelTime(double travelTime) {
+        this.travelTime = travelTime;
+    }*/
+    }
+
+
 
 
     
