2a92cb0da101e8a98c73d1f9025288c937885409
parhe <parhe@DESKTOP-4TBDPQR>
2017-11-18 23:18:48 -0700

    SceneControl 1.1

--- oregontrail1/src/oregontrail/control/SceneControl.java
+++ oregontrail1/src/oregontrail/control/SceneControl.java
@@ -4,11 +4,27 @@
  * and open the template in the editor.
  */
 package oregontrail.control;
-
 /**
  *
  * @author Kyle Jones
  */
 public class SceneControl {
     
+    public double sceneSelection( double distance, double time, double health)
+    {
+        
+        if( distance > 2500 || distance < 0 || time > 24 || time < 0 || health > 300 || health < 0 )
+        {
+            System.out.println("\nDistance must be greater than 0 and less than 2500 "
+                    + "and time must be valid (between 0 and 24"
+                    + "and health cannot be above full health and below 0 (death)");
+            return -1;
+        }
+                
+        double sceneSelection;
+        sceneSelection = 2500 - distance;
+        
+        return sceneSelection;
+        
+    }
 }
